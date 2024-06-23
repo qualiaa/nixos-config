b7b743bdcb4d081e13ed9c5f005cc135295eb149
Jamie Bayne <jamiebayne@hotmail.com>
2024-06-24 00:05:50 +0100

    Add pipewire configuration from default configuration.nix

--- profiles/pc/default.nix
+++ profiles/pc/default.nix
@@ -19,6 +19,23 @@
     };
   };
 
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
   # TODO: i3 configuration service module
   #       e.g. https://github.com/BirdeeHub/birdeeSystems/blob/582fe0c1123395c8cc0aa3a1bf6dfa3ce65dcfbb/common/i3/default.nix
   # Desktop, login and window managers
