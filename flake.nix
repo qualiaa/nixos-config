30a529fbdfe03db62edffc98c3b0cc230b447be6
Jamie Bayne <jamiebayne@hotmail.com>
2024-11-18 16:18:14 +0000

    Maintain separate channel for signal-desktop

--- flake.nix
+++ flake.nix
@@ -4,23 +4,32 @@
   inputs = {
     # NOTE: Pinned to unstable revision before amdgpu goes poof
     nixpkgs.url = "github:nixos/nixpkgs/12228ff";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }: let
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-hardware, ... }: let
 
     specialArgs = {
       inherit nixos-hardware;
       isDesktop = true;
     };
 
+    system = "x86_64-linux";
+
+    signal-overlay = final: prev: {
+      signal-desktop = nixpkgs-unstable.legacyPackages.${system}.signal-desktop;
+    };
+
   in
   {
     # TODO: Push modules into default.nix for each system
     nixosConfigurations.jamie-fw-nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
       specialArgs = specialArgs;
       modules = [
+        ({ ... }: { nixpkgs.overlays = [ signal-overlay ]; })
+
         nixos-hardware.nixosModules.framework-16-7040-amd
         ./hosts/fw16
         ./profiles/laptop
@@ -28,7 +37,7 @@
      ];
     };
     nixosConfigurations.jamie-xps-nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
       specialArgs = specialArgs;
       modules = [
         nixos-hardware.nixosModules.dell-xps-13-9370
