19a4a5b44aabcefdd2dd6e5dd71f857705446e4e
Jamie Bayne <jamiebayne@hotmail.com>
2025-03-24 23:15:09 +0000

    Enable hardware acceleration

--- hosts/fw16/default.nix
+++ hosts/fw16/default.nix
@@ -4,6 +4,11 @@
 
   networking.hostName = "jamie-fw-nixos";
 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
