ccb21225047214523380d80a685b9e27d2550441
Jamie Bayne <jamiebayne@hotmail.com>
2025-12-05 18:05:59 +0000

    Update pulse config and enable jack

--- profiles/pc/default.nix
+++ profiles/pc/default.nix
@@ -34,7 +34,7 @@
   };
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -42,7 +42,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
