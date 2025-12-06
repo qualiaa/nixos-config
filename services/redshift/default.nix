b1797360eaa3ad782850984b517a8887ae97f8c7
Jamie Bayne <jamiebayne@hotmail.com>
2025-12-06 14:48:08 +0000

    Add redshift hooks dependencies

--- services/redshift/default.nix
+++ services/redshift/default.nix
@@ -7,4 +7,15 @@
     temperature.day = 6700;
     temperature.night = 3000;
   };
+
+  # Needed by hooks
+  systemd.user.services.redshift.path = with pkgs; [
+    bash
+    gawk
+    glib
+    jq
+    kitty
+    procps
+    sway
+  ];
 }
